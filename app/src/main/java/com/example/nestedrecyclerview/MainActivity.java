ae2f4221f6db249a442d778314932e6540412868
andrei <git1@email.com>
2021-11-14 09:34:32 +0100

    Display data in Main Categories Recycler View
    * Display data in the Main Categories Recycler view inside of the Main Activity

--- app/src/main/java/com/example/nestedrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/nestedrecyclerview/MainActivity.java
@@ -1,14 +1,45 @@
 package com.example.nestedrecyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.example.nestedrecyclerview.adapter.MainRecyclerAdapter;
+import com.example.nestedrecyclerview.model.AllCategory;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mainCategoryRecycler;
+    MainRecyclerAdapter mainRecyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<AllCategory> allCategoryList = new ArrayList<>();
+        allCategoryList.add(new AllCategory("Hollywood"));
+        allCategoryList.add(new AllCategory("Best of Oscars"));
+        allCategoryList.add(new AllCategory("Movies Dubbed in Hindi"));
+        allCategoryList.add(new AllCategory("Category 4th"));
+        allCategoryList.add(new AllCategory("Category 5th"));
+
+        setMainCategoryRecycler(allCategoryList);
+
+    }
+
+    private void setMainCategoryRecycler(List<AllCategory> allCategoryList){
+
+        mainCategoryRecycler = findViewById(R.id.main_recycler);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mainCategoryRecycler.setLayoutManager(layoutManager);
+        mainRecyclerAdapter = new MainRecyclerAdapter(this, allCategoryList);
+        mainCategoryRecycler.setAdapter(mainRecyclerAdapter);
+
     }
 }
