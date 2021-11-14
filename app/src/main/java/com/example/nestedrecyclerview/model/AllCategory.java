673d96e59673ae542384cc7a8ca34658284b8283
andrei <git1@email.com>
2021-11-14 09:17:09 +0100

    Added Model Class for AllCategory
    * Added AllCategory.java Model Class for the list of Categories in the Main Recycler View inside of the Main Activity

---
+++ app/src/main/java/com/example/nestedrecyclerview/model/AllCategory.java
@@ -0,0 +1,18 @@
+package com.example.nestedrecyclerview.model;
+
+public class AllCategory {
+
+    String categoryTitle;
+
+    public AllCategory(String categoryTitle) {
+        this.categoryTitle = categoryTitle;
+    }
+
+    public String getCategoryTitle() {
+        return categoryTitle;
+    }
+
+    public void setCategoryTitle(String categoryTitle) {
+        this.categoryTitle = categoryTitle;
+    }
+}
