d4749b038c498e3e7e88beb1ce1cec6bf0211fc6
andrei <git1@email.com>
2021-11-14 10:09:42 +0100

    Display data in Hollywood Category
    * Display data in the Category Item Recycler View inside of the Main Categories Recycler View

--- app/src/main/java/com/example/nestedrecyclerview/MainActivity.java
+++ app/src/main/java/com/example/nestedrecyclerview/MainActivity.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 
 import com.example.nestedrecyclerview.adapter.MainRecyclerAdapter;
 import com.example.nestedrecyclerview.model.AllCategory;
+import com.example.nestedrecyclerview.model.CategoryItem;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,8 +23,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // added in first category
+        List<CategoryItem> categoryItemList = new ArrayList<>();
+        categoryItemList.add(new CategoryItem(1, R.drawable.hollywood1));
+        categoryItemList.add(new CategoryItem(1, R.drawable.hollywood2));
+        categoryItemList.add(new CategoryItem(1, R.drawable.hollywood3));
+        categoryItemList.add(new CategoryItem(1, R.drawable.hollywood4));
+        categoryItemList.add(new CategoryItem(1, R.drawable.hollywood5));
+        categoryItemList.add(new CategoryItem(1, R.drawable.hollywood6));
+
         List<AllCategory> allCategoryList = new ArrayList<>();
-        allCategoryList.add(new AllCategory("Hollywood"));
+        allCategoryList.add(new AllCategory("Hollywood", categoryItemList));
         allCategoryList.add(new AllCategory("Best of Oscars"));
         allCategoryList.add(new AllCategory("Movies Dubbed in Hindi"));
         allCategoryList.add(new AllCategory("Category 4th"));
